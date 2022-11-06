be6ca208ac5f107c096ff2c07012148221aab5a9
Natasha Matos <natasha.oliveirabusiness@gmail.com>
2022-11-05 22:09:25 -0300

    fix: insert authentication step in the game route of a poll

--- src/routes/game.ts
+++ src/routes/game.ts
@@ -1,39 +1,44 @@
 import { FastifyInstance } from "fastify";
 import { prisma } from "../lib/prisma";
 import { z } from "zod";
+import { authenticate } from "../plugins/authenticate";
 
 export async function GameRoutes(fastify: FastifyInstance) {
-  fastify.get("/polls/:pollId/games", async (request) => {
-    const getPoolParams = z.object({
-      pollId: z.string(),
-    });
+  fastify.get(
+    "/polls/:pollId/games",
+    { onRequest: [authenticate] },
+    async (request) => {
+      const getPoolParams = z.object({
+        pollId: z.string(),
+      });
 
-    const { pollId } = getPoolParams.parse(request.params);
+      const { pollId } = getPoolParams.parse(request.params);
 
-    const games = await prisma.game.findMany({
-      orderBy: {
-        date: "desc",
-      },
-      include: {
-        guesses: {
-          where: {
-            participant: {
-              userId: request.user.sub,
-              pollId: pollId,
+      const games = await prisma.game.findMany({
+        orderBy: {
+          date: "desc",
+        },
+        include: {
+          guesses: {
+            where: {
+              participant: {
+                userId: request.user.sub,
+                pollId: pollId,
+              },
             },
           },
         },
-      },
-    });
+      });
 
-    return {
-      games: games.map((game) => {
-        return {
-          ...game,
-          guess: game.guesses.length > 0 ? game.guesses[0] : null,
-          guesses: undefined,
-        };
-      }),
-    };
-  });
+      return {
+        games: games.map((game) => {
+          return {
+            ...game,
+            guess: game.guesses.length > 0 ? game.guesses[0] : null,
+            guesses: undefined,
+          };
+        }),
+      };
+    }
+  );
 }
